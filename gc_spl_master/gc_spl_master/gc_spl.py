323341cc418dcaf1fbf43147aacc3a93715b1788
Kenji Brameld <kenjibrameld@gmail.com>
2022-05-04 14:59:57 +0900

    add simple test
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- gc_spl_master/gc_spl_master/gc_spl.py
+++ gc_spl_master/gc_spl_master/gc_spl.py
@@ -7,12 +7,10 @@ class GCSPL(Node):
     def __init__(self):
         super().__init__('gc_spl')
 
-        self.data_port = self.get_parameter_or(
-            'data_port', 3838).get_parameter_value().integer_value
+        self.data_port = self.get_parameter_or('data_port', 3838)
         self.get_logger().info('data_port: "%s"' % self.data_port)
 
-        self.return_port = self.get_parameter_or(
-            'return_port', 3939).get_parameter_value().integer_value
+        self.return_port = self.get_parameter_or('return_port', 3939)
         self.get_logger().info('return_port: "%s"' % self.return_port)
 
 
--- gc_spl_master/test/test_gc_spl.py
+++ gc_spl_master/test/test_gc_spl.py
@@ -1,2 +1,8 @@
-def test_foo():
-    assert False
+import rclpy
+from gc_spl_master.gc_spl import GCSPL
+
+
+def test_node():
+    rclpy.init()
+    node = GCSPL()
+    rclpy.shutdown()
