c8bc7f564674eef747dd901ee289d5ac20e2d80d
Kenji Brameld <kenjibrameld@gmail.com>
2022-05-10 06:49:05 +0000

    convert ndarray to list, this seems to be not needed for newer distros
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- rcgcrd_4_conversion/rcgcrd_4_conversion/conversion.py
+++ rcgcrd_4_conversion/rcgcrd_4_conversion/conversion.py
@@ -21,13 +21,17 @@ from rcgcrd_4_conversion.robocup_game_control_return_data import RoboCupGameCont
 
 def rcgcrd_msg_to_data(msg: RCGCRD) -> bytes:
     """Convert RCGCRD ROS msg to binary data."""
+    # Must convert numpy.ndarray to list
+    pose = [msg.pose[0], msg.pose[1], msg.pose[2]]
+    ball = [msg.ball[0], msg.ball[1]]
+
     container = Container(
         playerNum=msg.player_num,
         teamNum=msg.team_num,
         fallen=msg.fallen,
-        pose=msg.pose,
+        pose=pose,
         ballAge=msg.ball_age,
-        ball=msg.ball
+        ball=ball
     )
     data = RoboCupGameControlReturnData.build(container)
     return data
