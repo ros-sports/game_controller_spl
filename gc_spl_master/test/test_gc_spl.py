896ba5c46c025bdb10842ac2ef3c34a1f2882b42
Kenji Brameld <kenjibrameld@gmail.com>
2022-06-10 19:59:46 +0900

    use SO_REUSEADDR instead of SO_REUSEPORT
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- gc_spl_master/test/test_gc_spl.py
+++ gc_spl_master/test/test_gc_spl.py
@@ -128,7 +128,7 @@ class TestGCSPL:
         # UDP Server - adapted from https://github.com/ninedraft/python-udp/blob/master/server.py
         server = socket.socket(
             socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         server.bind((socket.gethostbyname(socket.gethostname()), 3939))
         server.settimeout(0.1)
