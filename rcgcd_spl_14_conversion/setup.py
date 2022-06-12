28b45af50713eda613e610f1984f35209b7e2f68
Kenji Brameld <kenjibrameld@gmail.com>
2022-06-12 09:48:56 +0900

    update description to specify SPL
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>
    (cherry picked from commit 546cc190b228f6d81a1338982e04782d292a8f86)

--- rcgcd_spl_14_conversion/setup.py
+++ rcgcd_spl_14_conversion/setup.py
@@ -15,7 +15,7 @@ setup(
     zip_safe=True,
     maintainer='ijnek',
     maintainer_email='kenjibrameld@gmail.com',
-    description='Converts RoboCup GameController Data V14 between ROS msg and UDP raw bytes',
+    description='Converts RoboCup SPL GameController Data V14 between ROS msg and UDP raw bytes',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
