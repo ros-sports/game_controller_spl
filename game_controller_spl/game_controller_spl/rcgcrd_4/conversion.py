41a8d61f57d7c814c375d62c2521d9da9f53f06b
Kenji Brameld <kenjibrameld@gmail.com>
2024-01-20 17:05:44 +0000

    backport changes from 79, but without the deprecation of packages
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

---
+++ game_controller_spl/game_controller_spl/rcgcrd_4/conversion.py
@@ -0,0 +1,33 @@
+# Copyright 2022 Kenji Brameld
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from construct import Container
+
+from game_controller_spl.rcgcrd_4.robocup_game_control_return_data import \
+    RoboCupGameControlReturnData
+from game_controller_spl_interfaces.msg import RCGCRD4
+
+
+def rcgcrd_msg_to_data(msg: RCGCRD4) -> bytes:
+    """Convert RCGCRD ROS msg to binary data."""
+    container = Container(
+        playerNum=msg.player_num,
+        teamNum=msg.team_num,
+        fallen=msg.fallen,
+        pose=msg.pose,
+        ballAge=msg.ball_age,
+        ball=msg.ball
+    )
+    data = RoboCupGameControlReturnData.build(container)
+    return data
